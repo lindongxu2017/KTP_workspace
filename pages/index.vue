b74cc254f0eeb7f8c0bd06a1c4d85082150488b8
steven <1549482537@qq.com>
2021-07-15 11:13:35 +0800

    nuxt web-site-demo

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="index">
+    <div class="container">
+      <div class="row">
+        <div class="col">111</div>
+        <div class="col">222</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
